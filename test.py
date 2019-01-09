f276ac34e510759546116fcd1f26f40377dcf87d
Dilawar Singh <dilawars@ncbs.res.in>
2019-01-09 09:18:32 +0100

    Added test script.

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+try:
+    import tensorflow as tf
+except ImportError as e:
+    print( 'Failed to import tensorflow' )
+    raise e
+
+import deeplabcut as dc
+
+def main():
+    print( '[INFO] Testing ..' )
+
+
+if __name__ == "__main__":
+    main()
